0f061153bcc8c94e3013645989d5b4b18e9de481
roman <romanpz051@gmail.com>
2025-10-05 10:46:32 +0200

    0.0.7.01- Order user and Admin (Backend)

---
+++ apps/ecom-backend/src/main/java/com/rstefanyshyn/ecom/order/infrastructure/primary/order/RestOrderRead.java
@@ -0,0 +1,23 @@
+package com.rstefanyshyn.ecom.order.infrastructure.primary.order;
+
+
+import com.rstefanyshyn.ecom.order.domain.order.aggregate.Order;
+import com.rstefanyshyn.ecom.order.domain.order.vo.OrderStatus;
+import org.jilt.Builder;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record RestOrderRead(UUID publicId,
+                            OrderStatus status,
+                            List<RestOrderedItemRead> orderedItems) {
+
+  public static RestOrderRead from(Order order) {
+    return RestOrderReadBuilder.restOrderRead()
+      .publicId(order.getPublicId().value())
+      .status(order.getStatus())
+      .orderedItems(RestOrderedItemRead.from(order.getOrderedProducts()))
+      .build();
+  }
+}
